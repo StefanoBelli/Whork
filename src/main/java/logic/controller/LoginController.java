c1d6d44530dc9dc6591908493de13bd2cd19e255
ssynx <sa.belli@hotmail.it>
2021-02-10 12:38:06 +0100

    update

--- src/main/java/logic/controller/LoginController.java
+++ src/main/java/logic/controller/LoginController.java
@@ -1,15 +1,5 @@
 package logic.controller;
 
-import logic.model.UserModel;
-
 public final class LoginController {
 	private LoginController() {}
-	
-	public static String userBasicAuth(String username, String password) {
-		return null;
-	}
-	
-	public static UserModel validateCookie(String cookie) {
-		return null;
-	}
 }
--- src/test/java/test/controller/TestLoginController.java
+++ src/test/java/test/controller/TestLoginController.java
@@ -7,15 +7,6 @@ import org.junit.Test;
 import logic.controller.LoginController;
 
 public class TestLoginController {
-
-	@Test
-	public void testNullLogin() {
-		assertEquals(null, LoginController.userBasicAuth("", ""));
-	}
 	
-	@Test
-	public void testNullCookie() {
-		assertEquals(null, LoginController.validateCookie(""));
-	}
 
 }
