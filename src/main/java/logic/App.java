3e00330e14af8d13a23fe4e17be6ddb0d97e95ab
ssynx <sa.belli@hotmail.it>
2021-02-26 13:38:28 +0100

    refactoring

--- src/main/java/logic/App.java
+++ src/main/java/logic/App.java
@@ -36,10 +36,10 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 final class App {
-	//Logger tagged "WhorkStartup" for startup phase
+	// Logger tagged "WhorkStartup" for startup phase
 	private static final Logger LOGGER = LoggerFactory.getLogger("WhorkStartup");
-	
-	//Cmdline opts
+
+	// Cmdline opts
 	private static final String BASEOPT = "base";
 	private static final String WEBRESOPT = "webRes";
 	private static final String WEBROOTOPT = "webRoot";
@@ -55,11 +55,11 @@ final class App {
 	private static final String MAILNOTLSOPT = "mailNoTls";
 	private static final String MAILHOSTOPT = "mailHost";
 
-	//Self-extraction properties
+	// Self-extraction properties
 	private static ArrayList<String> webResDirectory = new ArrayList<>();
 	private static ArrayList<String> webResFiles = new ArrayList<>();
-	
-	//Dynamic config for whork
+
+	// Dynamic config for whork
 	private static String webRoot;
 	private static String base = "";
 	private static int port = 8080;
@@ -73,8 +73,8 @@ final class App {
 	private static String mailFrom = null;
 	private static String mailPwd = null;
 	private static String mailHost = null;
-	
-	//Static config for whork
+
+	// Static config for whork
 	private static final String DBNAME = "whorkdb";
 
 	private static final String NOT_USING_PWDAUTH = "NOT using password authentication";
@@ -164,55 +164,44 @@ final class App {
 	private static Options createOptions() {
 		Options opt = new Options();
 
-		//webapp
+		// webapp
 		opt.addOption(PORTOPT, true, new StringBuilder()
-				.append("Provide different port from the standard one (default: ")
-				.append(port)
-				.append(")")
-				.toString());
+				.append("Provide different port from the standard one (default: ").append(port).append(")").toString());
 
-		//webapp
-		opt.addOption(BASEOPT, true, new StringBuilder()
-				.append("Provide base path for website (default: ")
-				.append(base.isEmpty() ? "/" : base)
-				.append(")")
-				.toString());
+		// webapp
+		opt.addOption(BASEOPT, true, new StringBuilder().append("Provide base path for website (default: ")
+				.append(base.isEmpty() ? "/" : base).append(")").toString());
 
-		//webapp
-		opt.addOption(WEBROOTOPT, true, new StringBuilder()
-				.append("Provide different root directory for web resource extraction and usage (default: ")
-				.append(webRoot == null ? "webRes must be provided" : webRoot)
-				.append(")")
-				.toString());
-		
-		opt.addOption(DBCONNECTOPT, true, new StringBuilder()
-				.append("Where we can find your DB? \"hostname:[0-65535]\". We don't check syntax. (default: ")
-				.append(dbConnect)
-				.append(")")
-				.toString());
-		
-		opt.addOption(DBPWDOPT, true, new StringBuilder()
-				.append("Password for \"dbUser\". (default: ")
-				.append(dbPwd == null ? NOT_USING_PWDAUTH : dbPwd)
-				.append(")")
-				.toString());
-		
-		//webapp
-		opt.addOption(WEBRESOPT, true, "Provide web resources on your own (required if no self-extraction, default: none)");
-		
-		opt.addOption(DBUSEROPT, true, 
+		// webapp
+		opt.addOption(WEBROOTOPT, true,
+				new StringBuilder()
+						.append("Provide different root directory for web resource extraction and usage (default: ")
+						.append(webRoot == null ? "webRes must be provided" : webRoot).append(")").toString());
+
+		opt.addOption(DBCONNECTOPT, true,
+				new StringBuilder()
+						.append("Where we can find your DB? \"hostname:[0-65535]\". We don't check syntax. (default: ")
+						.append(dbConnect).append(")").toString());
+
+		opt.addOption(DBPWDOPT, true, new StringBuilder().append("Password for \"dbUser\". (default: ")
+				.append(dbPwd == null ? NOT_USING_PWDAUTH : dbPwd).append(")").toString());
+
+		// webapp
+		opt.addOption(WEBRESOPT, true,
+				"Provide web resources on your own (required if no self-extraction, default: none)");
+
+		opt.addOption(DBUSEROPT, true,
 				"User which has sufficient privileges to manage whork's DB. (required, default: none)");
-		
+
 		opt.addOption(DESKTOPOPT, false, "Launch desktop application");
-		
+
 		opt.addOption(MAILFROMOPT, true, "From email address (required)");
 
 		opt.addOption(MAILPASSWORDOPT, true, "Mail provider password authentication");
 
 		opt.addOption(MAILHOSTOPT, true, "Mail provider host (required, hostname or IP address)");
 
-		opt.addOption(MAILSMTPPORTOPT, true, new StringBuilder()
-				.append("Mail provider SMTP port (default: ")
+		opt.addOption(MAILSMTPPORTOPT, true, new StringBuilder().append("Mail provider SMTP port (default: ")
 				.append(mailSmtpPort).append(")").toString());
 
 		opt.addOption(MAILNOTLSOPT, false, "Disable TLS for mail provider");
@@ -221,63 +210,135 @@ final class App {
 
 		return opt;
 	}
-	
+
+	private static boolean assignBase(String arg, String value) {
+		base = value;
+		if (base.equals("/")) {
+			base = "";
+		} else if (base.charAt(0) != '/' || base.endsWith("/")) {
+			LOGGER.error("{} must start with / *AND* not end with /", arg);
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean assignServerPort(String arg, String value) {
+		port = Integer.parseInt(value);
+		if (!Util.isValidPort(port)) {
+			LOGGER.error("{} number must be within range [0-65535]", arg);
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean assignSmtpPort(String arg, String value) {
+		mailSmtpPort = value;
+		if (!Util.isValidPort(Integer.parseInt(mailSmtpPort))) {
+			LOGGER.error("{} number must be within range [0-65535]", arg);
+			return false;
+		}
+
+		return true;
+	}
+
+	private static void assignWebRootOnRes(String arg, String value) {
+		if (selfExtract) {
+			LOGGER.warn("ignoring {} value because self extraction is enabled...", arg);
+		} else {
+			webRoot = new File(value).getAbsolutePath();
+		}
+	}
+
+	private static void assignWebRootOnRoot(String arg, String value) {
+		if (!selfExtract) {
+			LOGGER.warn("ignoring {} property because self extraction is disabled...", arg);
+		} else {
+			webRoot = new File(value).getAbsolutePath();
+		}
+	}
+
 	private static boolean optCheck(Option opt) {
 		String argName = opt.getOpt();
-		if (argName != null) {
-			if (argName.equals(BASEOPT)) {
-				base = opt.getValue();
-				if (base.equals("/")) {
-					base = "";
-				} else {
-					if (base.charAt(0) != '/' || base.endsWith("/")) {
-						LOGGER.error("{} must start with / *AND* not end with /", argName);
-						return false;
-					}
-				}
-			} else if (argName.equals(PORTOPT)) {
-				port = Integer.parseInt(opt.getValue());
-				if (!Util.isValidPort(port)) {
-					LOGGER.error("{} number must be within range [0-65535]", argName);
-					return false;
-				}
-			} else if (argName.equals(WEBRESOPT)) {
-				if (selfExtract) {
-					LOGGER.warn("ignoring {} value because self extraction is enabled...", argName);
-				} else {
-					webRoot = new File(opt.getValue()).getAbsolutePath();
-				}
-			} else if (argName.equals(WEBROOTOPT)) {
-				if (!selfExtract) {
-					LOGGER.warn("ignoring {} property because self extraction is disabled...", argName);
-				} else {
-					webRoot = new File(opt.getValue()).getAbsolutePath();
-				}
-			} else if (argName.equals(DBCONNECTOPT)) {
-				dbConnect = opt.getValue();
-			} else if (argName.equals(DBUSEROPT)) {
-				dbUser = opt.getValue();
-			} else if (argName.equals(DBPWDOPT)) {
-				dbPwd = opt.getValue();
-			} else if (argName.equals(DESKTOPOPT)) {
-				launchDesktop = true;
-			} else if (argName.equals(MAILHOSTOPT)) {
-				mailHost = opt.getValue();
-			} else if (argName.equals(MAILNOTLSOPT)) {
-				mailTls = "false";
-			} else if (argName.equals(MAILSMTPPORTOPT)) {
-				mailSmtpPort = opt.getValue();
-				if (!Util.isValidPort(Integer.parseInt(mailSmtpPort))) {
-					LOGGER.error("{} number must be within range [0-65535]", argName);
-					return false;
-				}
-			}  else if (argName.equals(MAILFROMOPT)) {
-				mailFrom = opt.getValue();
-			} else if (argName.equals(MAILPASSWORDOPT)) {
-				mailPwd = opt.getValue();
-			}
+
+		if (argName == null) {
+			return true;
 		}
-		
+
+		if (argName.equals(BASEOPT) && 
+				!assignBase(argName, opt.getValue())) {
+			return false;
+		} else if (argName.equals(PORTOPT) && 
+				!assignServerPort(argName, opt.getValue())) {
+			return false;
+		} else if (argName.equals(WEBRESOPT)) {
+			assignWebRootOnRes(argName, opt.getValue());
+		} else if (argName.equals(WEBROOTOPT)) {
+			assignWebRootOnRoot(argName, opt.getValue());
+		} else if (argName.equals(DBCONNECTOPT)) {
+			dbConnect = opt.getValue();
+		} else if (argName.equals(DBUSEROPT)) {
+			dbUser = opt.getValue();
+		} else if (argName.equals(DBPWDOPT)) {
+			dbPwd = opt.getValue();
+		} else if (argName.equals(DESKTOPOPT)) {
+			launchDesktop = true;
+		} else if (argName.equals(MAILHOSTOPT)) {
+			mailHost = opt.getValue();
+		} else if (argName.equals(MAILNOTLSOPT)) {
+			mailTls = "false";
+		} else if (argName.equals(MAILSMTPPORTOPT) && 
+				!assignSmtpPort(argName, opt.getValue())) {
+			return false;
+		} else if (argName.equals(MAILFROMOPT)) {
+			mailFrom = opt.getValue();
+		} else if (argName.equals(MAILPASSWORDOPT)) {
+			mailPwd = opt.getValue();
+		}
+
+		return true;
+	}
+
+	private static String getPassword(String pwd) {
+		return pwd == null ? NOT_USING_PWDAUTH : HIDE_PWD;
+	}
+
+	private static void logParsedConfig() {
+		if (launchDesktop) {
+			LOGGER.info(
+					"Settings for Whork desktop:\n--> db: {}\n |--> dbuser:"
+							+ " {}\n |--> dbpwd: {}\n--> mailfrom: {}\n--> mailhost:"
+							+ " {}\n--> mailpwd: {}\n--> mailtls: {}\n--> smtpport: {}",
+					dbConnect, dbUser, getPassword(dbPwd), mailFrom, mailHost, getPassword(mailPwd), mailTls,
+					mailSmtpPort);
+		} else {
+			LOGGER.info(
+					"{}:\n--> port: {}\n--> base: {}\n--> webroot: {}\n"
+							+ "--> self-extract? {}\n--> db: {}\n |--> dbuser: {}\n"
+							+ " |--> dbpwd: {}\n--> mailfrom: {}\n--> mailhost: {}\n"
+							+ "--> mailpwd: {}\n--> mailtls: {}\n--> smtpport: {}",
+					"Settings for Whork webapp", port, base.isEmpty() ? "/" : base, webRoot, selfExtract, dbConnect,
+					dbUser, getPassword(dbPwd), mailFrom, mailHost, getPassword(mailPwd), mailTls, mailSmtpPort);
+		}
+	}
+
+	private static boolean configRequirementsChecks() {
+		if (dbUser == null) {
+			LOGGER.error("you must pass -{} in order to access DB", DBUSEROPT);
+			return false;
+		}
+
+		if (mailHost == null || mailFrom == null) {
+			LOGGER.error("you must pass -{} and -{} in order to be able to send emails", MAILFROMOPT, MAILHOSTOPT);
+			return false;
+		}
+
+		if (!launchDesktop && webRoot == null && !selfExtract) {
+			LOGGER.error("you must pass -{} in order to specify where to locate web resources", WEBRESOPT);
+			return false;
+		}
+
 		return true;
 	}
 
@@ -293,40 +354,15 @@ final class App {
 		}
 
 		for (Option option : cmd.getOptions()) {
-			if(!optCheck(option))
+			if (!optCheck(option))
 				return false;
 		}
 
-		if (dbUser == null) {
-			LOGGER.error("you must pass -{} in order to access DB", DBUSEROPT);
+		if (!configRequirementsChecks()) {
 			return false;
 		}
 
-		if(mailHost == null || mailFrom == null) {
-			LOGGER.error("you must pass -{} and -{} in order to be able to send emails", 
-						MAILFROMOPT, MAILHOSTOPT);
-			return false;
-		}
-		
-		if (!launchDesktop) {
-			if(webRoot == null && !selfExtract) {
-				LOGGER.error("you must pass -{} in order to specify where to locate web resources", WEBRESOPT);
-				return false;
-			}
-			
-			LOGGER.info("{}:\n--> port: {}\n--> base: {}\n--> webroot: {}\n" +
-			"--> self-extract? {}\n--> db: {}\n |--> dbuser: {}\n |--> dbpwd: {}\n" +
-			"--> mailfrom: {}\n--> mailhost: {}\n--> mailpwd: {}\n--> mailtls: {}\n--> smtpport: {}",
-					"Settings for Whork webapp", 
-					port, base.isEmpty() ? "/" : base, webRoot, selfExtract, dbConnect, dbUser, 
-					dbPwd == null ? NOT_USING_PWDAUTH : HIDE_PWD, mailFrom, mailHost, 
-					mailPwd == null ? NOT_USING_PWDAUTH : HIDE_PWD, mailTls, mailSmtpPort);
-		} else {
-			LOGGER.info("Settings for Whork desktop:\n--> db: {}\n |--> dbuser: {}\n |--> dbpwd: {}\n" +
-			"--> mailfrom: {}\n--> mailhost: {}\n--> mailpwd: {}\n--> mailtls: {}\n--> smtpport: {}",
-			dbConnect, dbUser, dbPwd == null ? NOT_USING_PWDAUTH : HIDE_PWD, mailFrom, mailHost, 
-					mailPwd == null ? NOT_USING_PWDAUTH : HIDE_PWD, mailTls, mailSmtpPort);
-		}
+		logParsedConfig();
 
 		return true;
 	}
@@ -344,7 +380,7 @@ final class App {
 
 	private static void cleanup() {
 		Logger cleanupLogger = LoggerFactory.getLogger("WhorkCleanup");
-		
+
 		if (!launchDesktop && selfExtract) {
 			cleanupLogger.info("deleting webroot...");
 			try {
@@ -354,16 +390,16 @@ final class App {
 				cleanupLogger.error("unable to delete webroot @ {}", webRoot);
 			}
 		}
-		
+
 		cleanupLogger.info("closing DB connection...");
-		
+
 		try {
 			Database.getInstance().closeConnection();
-		} catch(DatabaseException e) {
+		} catch (DatabaseException e) {
 			cleanupLogger.warn(e.getMessage());
 			cleanupLogger.warn("please report this, we don't expect this at this point. Something is broken :S");
 		}
-		
+
 		cleanupLogger.info("bye bye!");
 	}
 
@@ -378,7 +414,7 @@ final class App {
 		try {
 			ComuniDao.populatePool();
 			EmploymentStatusDao.populatePool();
-		} catch(DataAccessException e) {
+		} catch (DataAccessException e) {
 			exceptionMessageBeforeStart(e, e.getCause().getMessage());
 			return false;
 		}
@@ -399,7 +435,42 @@ final class App {
 		MailController.setMailSender(sender);
 	}
 
-	public static void main(String[] args) {
+	private static boolean attemptToEstablishDbConnection() {
+		LOGGER.info("Checking if we can correctly talk to DB...");
+		LOGGER.info("Driver: {}", Database.DRIVER);
+
+		DatabaseName.setDbName(DBNAME);
+
+		try {
+			Connection conn = Database.getInstance(dbConnect, dbUser, dbPwd).getConnection();
+			conn.setCatalog(DatabaseName.getDbName());
+			LOGGER.info("Yes, we're able to talk to DB!");
+		} catch (ClassNotFoundException e) {
+			exceptionMessageBeforeStart(e, "unable to load driver class, this SHOULD be reported!");
+			return false;
+		} catch (SQLException e) {
+			exceptionMessageBeforeStart(e, "unable to correctly establish a DB connection!");
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean initialize() {
+		if (!attemptToEstablishDbConnection()) {
+			return false;
+		}
+
+		if (!prePopulatePools()) {
+			return false;
+		}
+
+		setMailSender();
+
+		return true;
+	}
+
+	private static boolean parse(String[] args) {
 		try {
 			if (selfExtract) {
 				webRoot = new File("whork_webroot").getAbsolutePath();
@@ -408,61 +479,38 @@ final class App {
 			}
 
 			if (!propertySetup(args)) {
-				return;
+				return false;
 			}
-			
-			if(!launchDesktop) {
+
+			if (!launchDesktop) {
 				selfExtraction();
 			} else {
-				LOGGER.info("launching desktop application: any of {},{},{},{} options are ignored", 
-						BASEOPT, PORTOPT, WEBRESOPT, WEBROOTOPT);
+				LOGGER.info("launching desktop application: any of {},{},{},{} options are ignored", BASEOPT, PORTOPT,
+						WEBRESOPT, WEBROOTOPT);
 				LOGGER.info("launching desktop application: self extraction is ignored anyway");
 			}
 		} catch (ParseException e) {
 			exceptionMessageBeforeStart(e, "unable to parse command line");
-			return;
+			return false;
 		} catch (IOException e) {
 			exceptionMessageBeforeStart(e, "unable to correclty self-extract");
-			return;
+			return false;
 		}
 
+		return true;
+	}
+
+	private static void setRuntimeHooks() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
 				App.cleanup();
 			}
 		});
-		
-		LOGGER.info("Checking if we can correctly talk to DB...");
-		LOGGER.info("Driver: {}", Database.DRIVER);
-		
-		DatabaseName.setDbName(DBNAME);
-		
-		try {
-			Connection conn = Database
-					.getInstance(dbConnect, dbUser, dbPwd)
-					.getConnection();
-			
-			conn.setCatalog(DatabaseName.getDbName());
-			
-			LOGGER.info("Yes, we're able to talk to DB!");
-		} catch (ClassNotFoundException e) {
-			exceptionMessageBeforeStart(e, "unable to load driver class, this SHOULD be reported!");
-			return;
-		} catch (SQLException e) {
-			exceptionMessageBeforeStart(e, "unable to correctly establish a DB connection!");
-			return;
-		}
-		
-		if(!prePopulatePools()) {
-			return;
-		}
+	}
 
-		setMailSender();
-
-		LOGGER.info("Initialization phase completed!");
-		
-		if(!launchDesktop) {
+	private static void finalizeLaunch(String[] args) {
+		if (!launchDesktop) {
 			LOGGER.info("Welcome to Whork webapp! Starting up...");
 
 			if (!startTomcat()) {
@@ -471,6 +519,15 @@ final class App {
 		} else {
 			LOGGER.info("Welcome to Whork desktop! Starting up...");
 			WhorkDesktopLauncher.launchApp(args);
+		}
+	}
+
+	public static void main(String[] args) {
+		if (parse(args)) {
+			setRuntimeHooks();
+			if (initialize()) {
+				finalizeLaunch(args);
+			}
 		}
 	}
 }
