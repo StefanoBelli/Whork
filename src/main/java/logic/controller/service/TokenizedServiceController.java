0fccf8855f0d406937a29fb5bbe15f816b7a63cd
ssynx <sa.belli@hotmail.it>
2021-06-27 12:07:46 +0200

    less useless code

--- src/main/java/logic/controller/service/TokenizedServiceController.java
+++ src/main/java/logic/controller/service/TokenizedServiceController.java
@@ -32,11 +32,15 @@ public class TokenizedServiceController {
 
 	private static final String CONTENT_LENGTH = "Content-Length";
 	private static final String LISTEN_ADDR = Util.INADDR_ANY;
-	protected static final int VALID_TOKEN_INTVL = 
+	private static final int VALID_TOKEN_INTVL_INTEGER = 
 		Util.InstanceConfig.getInt(Util.InstanceConfig.KEY_SVC_INTVL_TOK); //secs
+	private static final String VALID_TOKEN_INTVL_STRING = 
+		Util.InstanceConfig.getString(Util.InstanceConfig.KEY_SVC_INTVL_TOK); //secs
+	
 
 	protected final Response invalidTokenResponse;
 	protected final Response genericErrorResponse;
+	protected static final int VALID_TOKEN_INTVL = VALID_TOKEN_INTVL_INTEGER;
 	private final StatelessProtocol statelessProtocol = new StatelessProtocol(this);
 	private final Map<String, Pair<String, Long>> validTokens = new HashMap<>();
 	private final int listenPort;
@@ -116,11 +120,11 @@ public class TokenizedServiceController {
 		return token;
 	}
 
-	protected final String queryToken(String tok) {
+	private final String queryToken(String tok) {
 		Date nowTime = new Date();
 		Pair<String, Long> token = validTokens.get(tok);
 
-		if(nowTime.getTime() - token.getSecond() > VALID_TOKEN_INTVL) {
+		if(nowTime.getTime() - token.getSecond() > VALID_TOKEN_INTVL_INTEGER) {
 			validTokens.remove(tok);
 			return null;
 		}
@@ -128,7 +132,9 @@ public class TokenizedServiceController {
 		return token.getFirst();
 	}
 	
-	private final String getTokenAssocUserEmail(String token) {
+	protected final String getTokenAssocUserEmailFromHeaders(Map<String, String> headers) {
+		String token = headers.get("Token");
+
 		if(token != null) {
 			return queryToken(token);
 		}
@@ -141,9 +147,7 @@ public class TokenizedServiceController {
 		Map<String, String> headers = request.getHeaders();
 
 		String token = headers.get("Token");
-		String userEmail = getTokenAssocUserEmail(token);
-
-		if(userEmail != null) {
+		if(token != null && queryToken(token) != null) {
 			String newToken = addOrRefreshToken(token, null);
 			if(newToken == null) {
 				return genericErrorResponse;
@@ -151,12 +155,13 @@ public class TokenizedServiceController {
 
 			Response okResponse = new Response();
 			okResponse.addHeaderEntry(CONTENT_LENGTH, Integer.toString(newToken.length()));
-			okResponse.addHeaderEntry("Expires-In", Integer.toString(VALID_TOKEN_INTVL));
+			okResponse.addHeaderEntry("Expires-In", VALID_TOKEN_INTVL_STRING);
 			okResponse.setStatus(Status.OK);
 			okResponse.setBody(newToken);
-			
+		
 			return okResponse;
 		}
+
 
 		return invalidTokenResponse;
 	}
