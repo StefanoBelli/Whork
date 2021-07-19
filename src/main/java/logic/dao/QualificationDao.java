fb60b2604c8877dad2f588616f9ca36fbf901f56
Mike-98 <57173151+Mike-98@users.noreply.github.com>
2021-07-19 18:20:12 +0200

    dao for offers

---
+++ src/main/java/logic/dao/QualificationDao.java
@@ -0,0 +1,54 @@
+package logic.dao;
+
+import logic.exception.DataAccessException;
+import logic.Database;
+import logic.model.QualificationModel;
+import logic.bean.QualificationBean;
+import logic.pool.QualificationPool;
+import logic.factory.ModelFactory;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+public final class QualificationDao {
+	private QualificationDao() {}
+
+	private static final Connection CONN = 
+		Database.getInstance().getConnection();
+	
+	private static final String STMT_MAIN_POPULATE_POOL = 
+		"{ call GetQualifications() }";
+
+	public static void populatePool() throws DataAccessException {
+		try (CallableStatement stmt = CONN.prepareCall(STMT_MAIN_POPULATE_POOL)) {
+			stmt.execute();
+
+			try (ResultSet rs = stmt.getResultSet()) {
+				List<QualificationBean> esl = new ArrayList<>();
+
+				while (rs.next()) {
+					QualificationBean m = new QualificationBean();
+					m.setQualify(rs.getString(1));
+					esl.add(m);
+				}
+
+				QualificationPool.setQualifications(esl);
+			}
+
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		}
+	}
+
+	public static QualificationModel getQualification(String name) {
+		for(final QualificationBean m : QualificationPool.getQualifications()) {
+			if(m.getQualify().equals(name))
+				return ModelFactory.buildQualificationModel(m);
+		}
+
+		return null;
+	}
+}
---
+++ src/main/java/logic/dao/TypeOfContractDao.java
@@ -0,0 +1,54 @@
+package logic.dao;
+
+import logic.exception.DataAccessException;
+import logic.Database;
+import logic.model.TypeOfContractModel;
+import logic.bean.TypeOfContractBean;
+import logic.pool.TypeOfContractPool;
+import logic.factory.ModelFactory;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+public final class TypeOfContractDao {
+	private TypeOfContractDao() {}
+
+	private static final Connection CONN = 
+		Database.getInstance().getConnection();
+	
+	private static final String STMT_MAIN_POPULATE_POOL = 
+		"{ call GetTypesOfContract() }";
+
+	public static void populatePool() throws DataAccessException {
+		try (CallableStatement stmt = CONN.prepareCall(STMT_MAIN_POPULATE_POOL)) {
+			stmt.execute();
+
+			try (ResultSet rs = stmt.getResultSet()) {
+				List<TypeOfContractBean> esl = new ArrayList<>();
+
+				while (rs.next()) {
+					TypeOfContractBean m = new TypeOfContractBean();
+					m.setContract(rs.getString(1));
+					esl.add(m);
+				}
+
+				TypeOfContractPool.setTypesOfContract(esl);
+			}
+
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		}
+	}
+
+	public static TypeOfContractModel getTypeOfContract(String name) {
+		for(final TypeOfContractBean m : TypeOfContractPool.getTypesOfContract()) {
+			if(m.getContract().equals(name))
+				return ModelFactory.buildTypeOfContractModel(m);
+		}
+
+		return null;
+	}
+}
