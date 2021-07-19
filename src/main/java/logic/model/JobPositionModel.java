2774928a2d2fb264b1b52ce108decfe94cf9730d
Mike-98 <57173151+Mike-98@users.noreply.github.com>
2021-07-19 17:06:12 +0200

    offers

---
+++ src/main/java/logic/model/JobPositionModel.java
@@ -0,0 +1,13 @@
+package logic.model;
+
+public final class JobPositionModel {
+	private String position;
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+}
