16dc1e11539bfa26634a366e8f02d8d60e0d2c67
Mike-98 <57173151+Mike-98@users.noreply.github.com>
2021-07-25 12:37:15 +0200

    update userModel

--- src/main/java/logic/model/UserModel.java
+++ src/main/java/logic/model/UserModel.java
@@ -9,10 +9,6 @@ public class UserModel {
 	private String cf;
 	private String photo;
 	private boolean employee;
-	private String website;
-	private String twitter;
-	private String facebook;
-	private String instagram;
 
 	public String getCf() {
 		return cf;
@@ -61,35 +57,5 @@ public class UserModel {
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
-	public String getWebsite() {
-		return website;
-	}
-
-	public void setWebsite(String website) {
-		this.website = website;
-	}
-
-	public String getTwitter() {
-		return twitter;
-	}
-
-	public void setTwitter(String twitter) {
-		this.twitter = twitter;
-	}
-
-	public String getFacebook() {
-		return facebook;
-	}
-
-	public void setFacebook(String facebook) {
-		this.facebook = facebook;
-	}
-
-	public String getInstagram() {
-		return instagram;
-	}
-
-	public void setInstagram(String instagram) {
-		this.instagram = instagram;
-	}
+	
 }
