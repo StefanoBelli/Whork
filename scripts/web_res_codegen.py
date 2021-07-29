30d1e8db5fd398a218c5878015258ab4695a7346
StefanoBelli <sa.belli@hotmail.it>
2021-07-29 09:59:49 +0200

    added web_res_codegen.py

---
+++ scripts/web_res_codegen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+target = "../res/web"
+target_len_inc = len(target) + 1
+
+for curdir, _, files in os.walk(target):
+    curdir = curdir[target_len_inc:]
+    print("webResDirectories.add(\"{}\");".format(curdir))
+    for file in files:
+        print("webResFiles.add(\"/{}/{}\");".format(curdir,file))
