ac29c27c473a50911cb6a245ee70077248ab57ac
StefanoBelli <sa.belli@hotmail.it>
2021-08-08 17:34:13 +0200

    add author comments

--- src/main/java/logic/exception/AlreadyExistantUserException.java
+++ src/main/java/logic/exception/AlreadyExistantUserException.java
@@ -1,8 +1,5 @@
 package logic.exception;
 
-/**
- * @author Stefano Belli
- */
 public final class AlreadyExistantUserException extends Exception {
 	private static final long serialVersionUID = -9091076565671245111L;
 }
--- src/main/java/logic/exception/SendMailException.java
+++ src/main/java/logic/exception/SendMailException.java
@@ -1,8 +1,5 @@
 package logic.exception;
 
-/**
- * @author Stefano Belli
- */
 public final class SendMailException extends Exception {
 	private static final long serialVersionUID = 6434476973415942731L;
 }
