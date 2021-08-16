4c65a9ab00128debbd2c63c824078d86239bd0c8
Monello1299 <magliari.elio08@gmail.com>
2021-08-16 17:16:41 +0200

    test account

---
+++ src/test/java/test/controller/TestAccountController.java
@@ -0,0 +1,164 @@
+package test.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import logic.bean.CandidatureBean;
+import logic.bean.CompanyBean;
+import logic.bean.ComuneBean;
+import logic.bean.EmploymentStatusBean;
+import logic.bean.OfferBean;
+import logic.bean.ProvinciaBean;
+import logic.bean.RegioneBean;
+import logic.bean.UserAuthBean;
+import logic.bean.UserBean;
+import logic.controller.AccountController;
+import logic.controller.CandidatureController;
+import logic.controller.OfferController;
+import logic.controller.RegisterController;
+import logic.exception.AlreadyExistantCompanyException;
+import logic.exception.AlreadyExistantUserException;
+import logic.exception.DataAccessException;
+import logic.exception.InternalException;
+import logic.exception.InvalidVatCodeException;
+import logic.factory.BeanFactory;
+import logic.util.Util;
+import logic.util.tuple.Pair;
+import test.Db;
+
+/**
+ * @author Magliari Elio
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestAccountController {
+	static UserBean userJobSeeker;
+
+	@BeforeClass
+	public static void initDb() 
+			throws ClassNotFoundException, SQLException, DataAccessException, 
+			InvalidVatCodeException, AlreadyExistantCompanyException, 
+			AlreadyExistantUserException, InternalException {
+
+		Db.init();
+
+		Util.InstanceConfig.setConf(Util.InstanceConfig.KEY_MAILTLS, false);
+		Util.InstanceConfig.setConf(Util.InstanceConfig.KEY_MAILHOST, "smtp.more.fake.than.this");
+		Util.InstanceConfig.setConf(Util.InstanceConfig.KEY_MAILFROM, "whork.noreply@gmail.com");
+		Util.InstanceConfig.setConf(Util.InstanceConfig.KEY_MAILSMTP_PORT, "587");
+
+		//Recruiter
+		CompanyBean company = new CompanyBean();
+		company = BeanFactory.buildCompanyBean("FRRTTR04T45A662J", 
+				"data/seide.png", "FERRARI", "00159560366");
+
+		UserBean user = new UserBean();
+		user.setAdmin(true);
+		user.setEmployee(true);
+		user.setRecruiter(true);
+		user.setCompany(company);
+		user.setName("name recruiter");
+		user.setSurname("surname recruiter");
+		user.setPhoneNumber("3333333333");
+		user.setCf("TPLPLT00A01D612T");
+
+		UserAuthBean userAuth = new UserAuthBean();
+		userAuth.setEmail("recruiter@gmail.com");
+		userAuth.setPassword("password");
+
+		try {
+			RegisterController.register(new Pair<>(user, userAuth));
+		} catch (InternalException e) { //see test method desc
+			if(!e.getMessage().equals("Unable to send you an email!")) {
+				throw e;
+			}
+		}
+
+		OfferBean offer = new OfferBean();
+		offer.setCompany(company);
+		offer.setDescription("descrizione offerta 1");
+		offer.setEmployee(user);
+		offer.setJobCategory(BeanFactory.buildJobCategoryBean("Engineering"));
+		offer.setJobPhysicalLocationFullAddress("via tuscolana 8");
+		offer.setJobPosition(BeanFactory.buildJobPositionBean("Engineer"));
+		offer.setOfferName("offer 1");
+		offer.setQualification(BeanFactory.buildQualificationBean("Master's degree"));
+		offer.setSalaryEUR(2100);
+		offer.setTypeOfContract(BeanFactory.buildTypeOfContractBean("Full Time"));
+		offer.setWorkShift("10:00 - 19:00");
+		
+		OfferController.postOffer(offer);
+
+		//JobSeeker
+		RegioneBean region = new RegioneBean();
+		region.setNome("Lazio");
+		ProvinciaBean province = new ProvinciaBean();
+		province.setSigla("FR");
+		province.setRegione(region);
+		ComuneBean comune = new ComuneBean();
+		comune.setCap("03039");
+		comune.setNome("Sora");
+		comune.setProvincia(province);
+		EmploymentStatusBean employmentStatus = new EmploymentStatusBean();
+		employmentStatus.setStatus("Student");
+
+		UserBean userBean = new UserBean();
+		userBean.setAdmin(false);
+		userBean.setEmployee(false);
+		userBean.setRecruiter(false);
+		userBean.setCompany(null);
+		userBean.setName("name");
+		userBean.setSurname("surname");
+		userBean.setNote(null);
+		userBean.setPhoto(null);
+		userBean.setPhoneNumber("3333333333");
+		userBean.setBirthday(new java.sql.Date(972647238));
+		userBean.setCf("TPLPLT00A01D612A");
+		userBean.setCv(null);
+		userBean.setComune(comune);
+		userBean.setBiography(null);
+		userBean.setEmploymentStatus(employmentStatus);
+		userBean.setWebsite(null);
+		userBean.setTwitter(null);
+		userBean.setFacebook(null);
+		userBean.setInstagram(null);
+
+		UserAuthBean userAuthBean = new UserAuthBean();
+		userAuthBean.setEmail("email@gmail.com");
+		userAuthBean.setPassword("password");
+
+		try {
+			RegisterController.register(new Pair<>(userBean, userAuthBean));
+		} catch (InternalException e) { //see test method desc
+			if(!e.getMessage().equals("Unable to send you an email!")) {
+				throw e;
+			}
+		}
+
+		CandidatureBean candidature = new CandidatureBean();
+		candidature.setJobSeeker(userBean);
+		candidature.setOffer(offer);
+
+		try {
+			CandidatureController.insertCandidature(candidature);
+		} catch (InternalException e) { //see test method desc
+			if(!e.getMessage().equals("Unable to send you an email!")) {
+				throw e;
+			}
+		}
+
+		userJobSeeker = userBean;
+
+	}
+
+	@Test
+	public void testAGetSeekerCandidature() throws InternalException {
+		assertEquals(1, AccountController.getSeekerCandidature(userJobSeeker).size());
+	}
+}
