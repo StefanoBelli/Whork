95ce08112f890ae13611709d16a5d77ee8670f02
Monello1299 <magliari.elio08@gmail.com>
2021-08-24 00:36:58 +0200

    test

--- src/test/java/test/controller/TestCandidatureController.java
+++ src/test/java/test/controller/TestCandidatureController.java
@@ -136,7 +136,7 @@ public class TestCandidatureController {
 				throw e;
 			}
 		}
-		assertEquals(1, OfferController.getOfferById(3).getClickStats());
+		assertEquals(1, OfferController.getOfferById(2).getClickStats());
 		
 	}
 	
